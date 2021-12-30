8b8759c1cec3c0dac36d96dc58a6b0ea2880ebec
Bruno <brunofas12@gmail.com>
2021-12-31 01:06:27 +0800

    feat: open-and-close the url tab by keycodes

--- components/minor/iframe-page.tsx
+++ components/minor/iframe-page.tsx
@@ -1,4 +1,5 @@
 import Drawer from './drawer'
+import {  useEffect, useCallback } from 'react'
 
 import { useRecoilState } from 'recoil'
 import { iframePageState } from '../../store/atom'
@@ -6,7 +7,37 @@ import { iframePageState } from '../../store/atom'
 const IframePage = () => {
     const [IframePage, setIframePageState] = useRecoilState(iframePageState)
 
+    //Setting up a function to open/close the drawer with keycodes
+    const onKeyDown  = useCallback(
+        ( e: KeyboardEvent) => {
+            if(e.key === 'Escape'){
+                setIframePageState({
+                    ...IframePage,
+                    isOpen: false,
+                })
+            } else if( e.key === 'c' || e.ctrlKey){
+                setIframePageState({
+                    ...IframePage,
+                    isOpen: true,
+                    iframeSrc: 'https://yomo-next-cursor-chat-app.vercel.app/',
+                })
+            }
+            
+        }, [])
+
+        useEffect(()=> {
+
+            document.addEventListener('keydown', onKeyDown)
+
+            return () => {
+                document.removeEventListener('keydown', onKeyDown)
+           
+            }
+            
+        },[])
+
     return (
+        <>
         <Drawer
             isOpen={IframePage.isOpen}
             onClose={() =>
@@ -16,8 +47,10 @@ const IframePage = () => {
                 })
             }
         >
-            <iframe title='' width='100%' height='100%' src={IframePage.iframeSrc} />
+            <iframe title=''  width='100%' height='100%' src={IframePage.iframeSrc} />
         </Drawer>
+        
+        </>
     )
 }
 
